93d7e2ac821444aaf32c2557132bdfc825fcd9b0
Rick Dawson <dawsonra@clockworx.org>
2021-08-23 07:29:31 -0400

    add install script

--- install-wg.sh
+++ install-wg.sh
@@ -1,14 +1,12 @@
 # Install instructions for clients created by add-client.sh
-
-https://www.wireguard.com/install/
+# (C) 2021 Richard Dawson 
 
 # Ubuntu
 sudo add-apt-repository ppa:wireguard/wireguard
 
 # Debian
-echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable-wireguard.list
-printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' > /etc/apt/preferences.d/limit-unstable
-
+#echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable-wireguard.list
+#printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' > /etc/apt/preferences.d/limit-unstable
 
 #Both
 sudo apt-get update && sudo apt-get install wireguard
