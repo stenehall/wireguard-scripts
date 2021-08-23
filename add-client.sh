eacc67e489e83c7faf630b5b287e7d43d703a94d
Rick Dawson <dawsora@gmail.com>
2021-08-23 15:37:55 -0400

    Temporary fix to get ip address on vm only

--- add-client.sh
+++ add-client.sh
@@ -10,8 +10,9 @@ else
 	key=$(cat clients/$1/$1.priv) 
 	ip="10.8.0."$(expr $(cat last-ip.txt | tr "." " " | awk '{print $4}') + 1)
 	FQDN=$(hostname -f)
-  SERVER_PUB_KEY=$(cat /etc/wireguard/server_public_key)
-  cat wg0-client.example.conf | sed -e 's/:CLIENT_IP:/'"$ip"'/' | sed -e 's|:CLIENT_KEY:|'"$key"'|' | sed -e 's|:SERVER_PUB_KEY:|'"$SERVER_PUB_KEY"'|' | sed -e 's|:SERVER_ADDRESS:|'"$FQDN"'|' > clients/$1/wg0.conf
+	HOSTIP=$(ip -4 addr show enp1s0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+    SERVER_PUB_KEY=$(cat /etc/wireguard/server_public_key)
+    cat wg0-client.example.conf | sed -e 's/:CLIENT_IP:/'"$ip"'/' | sed -e 's|:CLIENT_KEY:|'"$key"'|' | sed -e 's|:SERVER_PUB_KEY:|'"$SERVER_PUB_KEY"'|' | sed -e 's|:SERVER_ADDRESS:|'"$HOSTIP"'|' > clients/$1/wg0.conf
 	echo $ip > last-ip.txt
 	cp install-client.sh clients/$1/install-client.sh
 	tar czvf clients/$1.tar.gz clients/$1
@@ -22,4 +23,5 @@ else
 	echo $ip" "$1 | sudo tee -a /etc/hosts
 	sudo wg show
 	qrencode -t ansiutf8 < clients/$1/wg0.conf
+	qrencode -0 clients/$1/$1.png < clients/$1/wg0.conf
 fi
--- install-server.sh
+++ install-server.sh
@@ -8,9 +8,14 @@ sudo apt-get -y install wireguard
 sudo apt-get install -y qrencode
 
 #Create Server Keys
-sudo sudo
+sudo su
 cd /etc/wireguard
 wg genkey | tee server_private_key | wg pubkey > server_public_key
 
 # Get config
 wget https://raw.githubusercontent.com/rdbh/wireguard-scripts/master/wg0-server.example.conf /etc/wireguard/wg0.conf
+
+# Get run scripts/master/wg0-server
+wget https://raw.githubusercontent.com/rdbh/wireguard-scripts/master/add-client.sh ~
+wget https://raw.githubusercontent.com/rdbh/wireguard-scripts/master/install-client.sh ~
+wget https://raw.githubusercontent.com/rdbh/wireguard-scripts/master/remove-peer.sh ~
