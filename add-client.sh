3444e2d5e9cc39537f77427b62e9594f5b7f7219
Johan Stenehall <stenehall@gmail.com>
2021-09-06 16:41:04 +0200

    Update add-client.sh

--- add-client.sh
+++ add-client.sh
@@ -9,15 +9,9 @@ else
 	wg genkey | tee clients/$1/$1.priv | wg pubkey > clients/$1/$1.pub
 	key=$(cat clients/$1/$1.priv) 
 	ip="10.100.200."$(expr $(cat last-ip.txt | tr "." " " | awk '{print $4}') + 1)
-	FQDN=$(hostname -f)
-	HOSTIP=$(ip -4 addr show enp1s0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
-	if [[ ${HOSTIP} == "" ]]
-	then
-	    echo "IP not found automatically. Update wg0.conf before adding clients"
-		HOSTIP="<Insert IP HERE>"
-	fi
-    SERVER_PUB_KEY=$(cat /etc/wireguard/server_public_key)
-    cat /etc/wireguard/wg0-client.example.conf | sed -e 's/:CLIENT_IP:/'"$ip"'/' | sed -e 's|:CLIENT_KEY:|'"$key"'|' | sed -e 's|:SERVER_PUB_KEY:|'"$SERVER_PUB_KEY"'|' | sed -e 's|:SERVER_ADDRESS:|'"$HOSTIP"'|' > clients/$1/wg0.conf
+	public_ip=$(curl ifconfig.io/ip -4)
+    	SERVER_PUB_KEY=$(cat /etc/wireguard/server_public_key)
+    	cat /etc/wireguard/wg0-client.example.conf | sed -e 's/:CLIENT_IP:/'"$ip"'/' | sed -e 's|:CLIENT_KEY:|'"$key"'|' | sed -e 's|:SERVER_PUB_KEY:|'"$SERVER_PUB_KEY"'|' | sed -e 's|:SERVER_ADDRESS:|'"$public_ip"'|' > clients/$1/wg0.conf
 	echo $ip > /etc/wireguard/last-ip.txt
 	cp install-client.sh clients/$1/install-client.sh
 	zip -r clients/$1.zip clients/$1
